69a39bbcd5022c33f34da30849994c5ac9e88e93
code8d <code8d@gmail.com>
2022-10-27 19:25:18 +0500

    Small changes in the modal window

--- js/script.js
+++ js/script.js
@@ -10,3 +10,30 @@ linkText.forEach(link => {
         link.classList.remove('active')
     }
 })
+
+const modal = document.querySelector('.modal-wrapper')
+const order = document.querySelectorAll('.bag-block')
+const close = document.querySelector('.close')
+const bg = document.querySelector('.bg')
+const header = document.querySelector('.header')
+
+order.forEach(ord => {
+    ord.addEventListener('click', () => {
+
+        modal.classList.add('show')
+    })
+})
+
+modal.addEventListener('click', () => {
+    console.log('work')
+})
+
+close.addEventListener('click', () => {
+    modal.classList.remove('show')
+})
+bg.addEventListener('click', () => {
+    modal.classList.remove('show')
+})
+header.addEventListener('click', () => {
+    modal.classList.remove('show')
+})
